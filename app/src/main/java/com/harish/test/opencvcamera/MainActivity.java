bd1d1003ea01187ba2838633211f7b051be91a15
Harish Veeramani <hdv987@gmail.com>
2018-01-14 13:31:05 -0600

    Add lifecycle for camera

--- app/src/main/java/com/harish/test/opencvcamera/MainActivity.java
+++ app/src/main/java/com/harish/test/opencvcamera/MainActivity.java
@@ -2,12 +2,71 @@ package com.harish.test.opencvcamera;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+    JavaCameraView mJavaCameraView;
+    BaseLoaderCallback mBaseLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status){
+                case BaseLoaderCallback.SUCCESS:{
+                    mJavaCameraView.enableView();
+                    break;
+                }
+                default:{
+                    super.onManagerConnected(status);
+                    break;
+                }
+            }
+        }
+    };
+
+    static {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mJavaCameraView = findViewById(R.id.java_camera_view);
+        mJavaCameraView.setVisibility(SurfaceView.VISIBLE);
+        mJavaCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mJavaCameraView != null){
+            mJavaCameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if (mJavaCameraView != null){
+            mJavaCameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(OpenCVLoader.initDebug()){
+            Log.d("MainActivity", "OpenCV Loaded");
+            mBaseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        } else {
+            Log.d("MainActivity", "NOT LOADED");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, this, mBaseLoaderCallback);
+        }
     }
 }
