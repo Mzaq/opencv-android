53920304795276321660f16d529c86b3146dda88
Harish Veeramani <hdv987@gmail.com>
2018-01-14 14:22:27 -0600

    Test camera

--- app/src/main/java/com/harish/test/opencvcamera/MainActivity.java
+++ app/src/main/java/com/harish/test/opencvcamera/MainActivity.java
@@ -1,17 +1,24 @@
 package com.harish.test.opencvcamera;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.SurfaceView;
 
 import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
     JavaCameraView mJavaCameraView;
+    Mat mRGBA;
     BaseLoaderCallback mBaseLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
@@ -40,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         mJavaCameraView = findViewById(R.id.java_camera_view);
         mJavaCameraView.setVisibility(SurfaceView.VISIBLE);
         mJavaCameraView.setCvCameraViewListener(this);
+
     }
 
     @Override
@@ -68,5 +76,31 @@ public class MainActivity extends AppCompatActivity {
             Log.d("MainActivity", "NOT LOADED");
             OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, this, mBaseLoaderCallback);
         }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRGBA = new Mat(height, width, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRGBA.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        //Fix camera orientation for portrait mode
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            mRGBA = inputFrame.rgba();
+            Mat mRGBA_T = mRGBA.t();
+            Core.transpose(mRGBA,mRGBA_T);
+            Core.flip(mRGBA_T, mRGBA_T, 1);
+            Imgproc.resize(mRGBA_T, mRGBA_T, mRGBA.size());
+
+            return mRGBA_T;
+        }
+        mRGBA = inputFrame.rgba();
+        return mRGBA;
     }
 }
